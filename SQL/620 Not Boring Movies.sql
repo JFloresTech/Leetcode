bc6bb879f3c0b738045745df4a607c6645fa8276
JFloresTech <156546178+JFloresTech@users.noreply.github.com>
2024-07-17 13:20:26 -0500

    Update 620 Not Boring Movies.sql

--- SQL/620 Not Boring Movies.sql
+++ SQL/620 Not Boring Movies.sql
@@ -55,33 +55,23 @@ GO
 USE [Leetcode_620]
 GO
 
-CREATE TABLE Signups ([user_id] int, time_stamp datetime)
-CREATE TABLE Confirmations ([user_id] int, time_stamp datetime, [action] VARCHAR(9) CHECK( [action] IN ('confirmed','timeout')))
+CREATE TABLE cinema (id int, movie varchar(255), description varchar(255), rating numeric(2, 1))
 
-INSERT INTO Signups ([user_id], time_stamp) values ('3', '2020-03-21 10:16:13')
-INSERT INTO Signups ([user_id], time_stamp) values ('7', '2020-01-04 13:57:59')
-INSERT INTO Signups ([user_id], time_stamp) values ('2', '2020-07-29 23:09:44')
-INSERT INTO Signups ([user_id], time_stamp) values ('6', '2020-12-09 10:39:37')
-
-INSERT INTO Confirmations (user_id, time_stamp, action) values ('3', '2021-01-06 03:30:46', 'timeout')
-INSERT INTO Confirmations (user_id, time_stamp, action) values ('3', '2021-07-14 14:00:00', 'timeout')
-INSERT INTO Confirmations (user_id, time_stamp, action) values ('7', '2021-06-12 11:57:29', 'confirmed')
-INSERT INTO Confirmations (user_id, time_stamp, action) values ('7', '2021-06-13 12:58:28', 'confirmed')
-INSERT INTO Confirmations (user_id, time_stamp, action) values ('7', '2021-06-14 13:59:27', 'confirmed')
-INSERT INTO Confirmations (user_id, time_stamp, action) values ('2', '2021-01-22 00:00:00', 'confirmed')
-INSERT INTO Confirmations (user_id, time_stamp, action) values ('2', '2021-02-28 23:59:59', 'timeout')
+INSERT INTO cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9')
+INSERT INTO cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5')
+INSERT INTO cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2')
+INSERT INTO cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6')
+INSERT INTO cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1')
 GO
 
 -----------------------
 --SOLUTION
 -----------------------
  
-SELECT [Signups].[user_id] AS [user_id], 
-		CAST(AVG(CASE WHEN [Confirmations].[action] = 'confirmed' THEN 1.00 ELSE 0.00 END) AS numeric(4,2)) AS [confirmation_rate]
-FROM [Signups]
-LEFT JOIN [Confirmations]
-ON [Signups].[user_id] = [Confirmations].[user_id]
-GROUP BY [Signups].[user_id]
+SELECT id, movie, description, rating
+FROM [cinema]
+WHERE (id%2 = 1) AND (description != 'boring')
+ORDER BY rating DESC
 
 /* REMOVE 
  USE master
