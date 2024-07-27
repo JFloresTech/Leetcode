5c3516131020469fbc54470d470eac3589042171
JFloresTech <156546178+JFloresTech@users.noreply.github.com>
2024-07-27 09:27:52 -0500

    1045

--- (Medium) 1045 Customers Who Bought All Products.sql
+++ (Medium) 1045 Customers Who Bought All Products.sql
@@ -87,7 +87,10 @@ GO
 --SOLUTION
 -----------------------
 
-
+SELECT customer_id
+FROM [Customer]
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product)
 
 /* REMOVE 
  USE master
